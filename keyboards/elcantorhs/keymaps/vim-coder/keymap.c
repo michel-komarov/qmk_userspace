e681a44570e1bd1c0d9a6b36b4c20ce3c6e39545
Michel Komarov <michel.komarov@gmail.com>
2024-02-18 18:17:40 +0300

    Add mouse wheel keys

--- keyboards/elcantorhs/keymaps/vim-coder/keymap.c
+++ keyboards/elcantorhs/keymaps/vim-coder/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_NO,        KC_NO,        KC_TRNS,      KC_NO,   /* | */    KC_SPC,       MO(3),        KC_ENT,       KC_MINS
     ),
     [2] = LAYOUT(
-        KC_NO,          KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_PSCR, /* | */    KC_NO,        KC_HOME,      KC_PGUP,      KC_NO,        KC_NO,        KC_INS,
+        KC_NO,          KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_PSCR, /* | */    KC_NO,        KC_HOME,      KC_PGUP,      KC_WH_U,      KC_NO,        KC_INS,
         KC_ESC,         KC_NO,         KC_LCTL,      KC_LALT,      KC_LSFT,      KC_NO,   /* | */    KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT,      KC_NO,        KC_DEL,
-        KC_TAB,         KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,   /* | */    KC_NO,        KC_END,       KC_PGDN,      KC_NO,        KC_NO,        KC_NO,
+        KC_TAB,         KC_NO,         KC_NO,        KC_WH_L,      KC_WH_R,      KC_NO,   /* | */    KC_NO,        KC_END,       KC_PGDN,      KC_WH_D,      KC_NO,        KC_NO,
                                        KC_CAPS,      KC_LGUI,      MO(3),        KC_BSPC, /* | */    KC_NO,        KC_TRNS,      KC_NO,        KC_NO
     ),
     [3] = LAYOUT(
         KC_F1,          KC_F2,         KC_F3,        KC_F4,        KC_F5,        KC_F6,   /* | */    KC_F7,        KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,
         KC_ESC,         KC_LGUI,       KC_LCTL,      KC_LALT,      KC_LSFT,      KC_NO,   /* | */    KC_NO,        KC_RSFT,      KC_RALT,      KC_RCTL,      KC_RGUI,      KC_NO,
         KC_TAB,         KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,   /* | */    KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-                                       KC_NO,        KC_NO,        KC_TRNS,      KC_NO,   /* | */    KC_NO,        KC_TRNS,      KC_NO,        KC_NO
+                                       QK_BOOT,      KC_NO,        KC_TRNS,      KC_NO,   /* | */    KC_NO,        KC_TRNS,      KC_NO,        ANY(QK_RBT)
     )
 };
